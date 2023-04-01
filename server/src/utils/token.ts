c6a1daa31a1e5a6866033badcd62398e063accca
an678-mhg <an567008@gmail.com>
2023-04-01 21:28:13 +0700

    change time accessToken in server

--- server/src/utils/token.ts
+++ server/src/utils/token.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (payload: object) => {
   return jwt.sign(payload, process.env.JWT_SECRET as string, {
-    expiresIn: "10s",
+    expiresIn: "1d",
   });
 };
 
